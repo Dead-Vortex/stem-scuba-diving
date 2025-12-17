c25f371378a07289eccefa2da5a9a1a654cce0d4
Dead-Vortex <milesohalpin@gmail.com>
2025-12-17 13:56:02 -0800

    updated npc and added map borders
    npc now has button prompt, new art, and smaller interact zone (to avoid softlocks)
    commits will now also update the build unless something is horribly broken

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var interact_zone : Area2D = $InteractZone
+@onready var button : Sprite2D = $ButtonPrompt
 @export var player : CharacterBody2D
 var is_player_interactable : bool = false
 var is_player_interacting : bool = false
@@ -9,17 +10,21 @@ func _process(_delta):
 	if Input.is_action_just_pressed("interact") and is_player_interactable:
 		if !is_player_interacting:
 			is_player_interacting = true
+			button.visible = false
 			interaction_menu()
 		else:
 			is_player_interacting = false
 
 func interaction_menu():
-	pass
+	player.facing = "right"
+	player.sprite.flip_h = false
 
 func _player_enters_npc_interact_zone(body):
 	if body == player:
 		is_player_interactable = true
+		button.visible = true
 
 func _player_exits_npc_interact_zone(body):
 	if body == player:
 		is_player_interactable = false
+		button.visible = false
