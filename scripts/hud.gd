a8097063bb2e1840e48bdc80b1e3faaf87a60d6b
Dead-Vortex <milesohalpin@gmail.com>
2026-01-10 19:28:01 -0800

    updated water gradient, fixed mobile controls

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -3,11 +3,7 @@ extends Node
 @onready var oxygen_display : RichTextLabel = $HBoxContainer/Oxygen
 @onready var trash_display : RichTextLabel = $HBoxContainer/Trash
 @export var player : CharacterBody2D
-var touch_ui = preload("res://scenes/mobile_ui.tscn")
 
-func _ready() -> void:
-	if OS.has_feature("mobile"):
-		add_sibling(touch_ui.instantiate())
 
 func _process(_delta : float) -> void:
 	oxygen_display.text = str(player.oxygen)
